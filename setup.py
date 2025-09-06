9379c21ebf4a5c69097697c485085eba23e3a845
vasundhrasingh25 <vasu250300@gmail.com>
2025-09-06 11:12:12 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="MediCan",
+    version="0.1.0",
+    author="Vasundhra",
+    author_email="vasundhrasingh2501@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
